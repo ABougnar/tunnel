5fa3adac151d7e00d690f2be559e0cb93c719d5a
ABougnar <71180066+ABougnar@users.noreply.github.com>
2020-09-14 14:17:28 +0200

    Update starter.sh

--- starter/starter.sh
+++ starter/starter.sh
@@ -36,7 +36,7 @@ echo ""
 ########
 clear
 echo  " "
-printf "  \e[101m\e[1;77m #> Script By Noob Hackers <# \e[0m\n"
+printf "  \e[101m\e[1;77m #> Script By Anas Bougnar <# \e[0m\n"
 printf "\n"
 printf " \e[120m\e[1;33m >> Enter Port Number ✈ [ 4 digit] : \e[0m\n"
 echo " "
